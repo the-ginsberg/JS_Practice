dff48d494aa81e9195f4aa121180f51f103c20eb
Jeff Ginsberg <jeffginsberg87@gmail.com>
2015-08-30 16:43:50 -0400

    Problem Solving with Loops

--- javascript.js
+++ javascript.js
@@ -11,3 +11,14 @@ while(num >= 1){
   console.log(num);
   num--;
 }
+
+// Problem Solving with Loops
+var numSheep = 4;
+var monthNumber = 1;
+var monthsToPrint = 12;
+
+while(monthNumber <= monthsToPrint){
+  numSheep = (numSheep * 4);
+  console.log("There will be " + numSheep + " sheep after " + monthNumber + " month(s)!");
+  monthNumber++;
+}
