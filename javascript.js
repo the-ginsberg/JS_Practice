d1e23a24fd846fada2a1271dedaf8198332c1aa6
Jeff Ginsberg <jeffginsberg87@gmail.com>
2015-08-30 17:12:14 -0400

    Hoover Dam Loop Problem

--- javascript.js
+++ javascript.js
@@ -37,3 +37,18 @@ for(monthNumber = 1; monthNumber <= monthsToPrint; monthNumber++){
   numSheep = (numSheep * 4);
   console.log("There will be " + numSheep + " sheep after " + monthNumber + " month(s)!");
 }
+
+// HooverDam Generator Problem
+var currentGen = 1;
+var totalGen = 19;
+var totalMW = 0;
+while(currentGen < 5){
+  totalMW = totalMW + 62;
+  console.log("Generator #" + currentGen + " is on, adding 62 MW, for a total of " + totalMW + " MW!");
+  currentGen++;
+}
+
+for(currentGen = 5; currentGen <= totalGen; currentGen++){
+  totalMW = totalMW + 124;
+  console.log("Generator #" + currentGen + " is on, adding 124 MW, for a total of " + totalMW + " MW!");
+}
