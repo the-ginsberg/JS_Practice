e281fd4ee821a07d0e0a6cd6085ca41b86ded0da
Jeff Ginsberg <jeffginsberg87@gmail.com>
2015-08-30 16:35:40 -0400

    descending loop

--- javascript.js
+++ javascript.js
@@ -1,5 +1,13 @@
+// Ascending Loop
 var number = 1;
 while(number <= 5){
   console.log(number);
   number++;
+}
+
+// Descending Loop
+var num = 10;
+while(num >= 1){
+  console.log(num);
+  num--;
 }
